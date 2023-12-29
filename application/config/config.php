634d382c6fe307b75ef08c264daa07eed8793cf7
kuriyantoadi <melatikuning212@gmail.com>
2023-12-29 20:27:23 +0700

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = '';
+// $config['base_url'] = 'https://wolvjacket.dukunglinux.my.id/';
 // $config['base_url'] = '';
 $root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
 $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
